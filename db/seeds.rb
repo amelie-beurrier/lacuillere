f484988506cfe1342143d99069e63f770a74045e
Amelie Beurrier <abeurrier@gmail.com>
2015-02-11 16:56:14 +0100

    back end restos app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "0101010101",
+    category:     "french"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number: "0202020202",
+    category:     "belgian"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number: "0101010103",
+    category:     "italian"
+  },
+  {
+    name:         "Nanashi",
+    address:      "rue Carré 75020 Paris",
+    phone_number: "0101010104",
+    category:     "japanese"
+  },
+  {
+    name:         "La Tour du Bonheur",
+    address:      "route de Paris 78000 Versailles",
+    phone_number: "0101010105",
+    category:     "chinese"
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
